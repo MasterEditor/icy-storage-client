9eddbbfb56c445638f1685ec9330fc463bda1ca6
MasteEditor <until_work@mail.ru>
2022-02-22 08:51:22 +0300

    Add Dependency Injection

---
+++ ViewModels/Implementations/ChromeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using HandyControl.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IcyStorageClient.ViewModels
+{
+    public class ChromeViewModel : BindablePropertyBase, IChromeViewModel
+    {
+        public IMainViewModel Main { get; }
+
+
+        private OverlayViewModel _overlay;
+        public OverlayViewModel Overlay
+        {
+            get => _overlay;
+            set { Set(ref _overlay, value); }
+        }
+    }
+}
